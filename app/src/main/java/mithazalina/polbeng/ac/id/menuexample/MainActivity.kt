30e4fa7eaf58e665dbb46b6778b86e16c86f95db
MithaZalinaRamadhany <mitazalina974@gmail.com>
2024-10-28 15:48:51 +0700

    push stater project

--- app/src/main/java/mithazalina/polbeng/ac/id/menuexample/MainActivity.kt
+++ app/src/main/java/mithazalina/polbeng/ac/id/menuexample/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : AppCompatActivity() {
                 return true
             }
             R.id.menuExit -> {
-                showMessage("Exit Menu")
+                showMessage("Exit ")
                 return true
             }
         }
